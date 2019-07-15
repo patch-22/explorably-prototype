cbbf31573993736acd3906c3d4c613982f47d906
Adam Kelly <adamkelly2201@gmail.com>
2019-07-15 16:39:08 +0100

    Added ThreeJS Demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './App.css'
-import Demo from './Demo'
+import Demo from './ThreeDemo'
 
 class App extends React.Component {
   render() {
