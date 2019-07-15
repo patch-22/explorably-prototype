801bcfc3c63d918dcaf141091b35db57279f28c1
Adam Kelly <adamkelly2201@gmail.com>
2019-07-15 17:08:45 +0100

    Improved ThreeJS Demo

--- src/ThreeDemo.js
+++ src/ThreeDemo.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react'
 import * as THREE from 'three'
 
 export default class ThreeDemo extends Component {
+  constructor(props) {
+    super(props)
+    this.mainRef = React.createRef()
+  }
+
   componentDidMount() {
     const width = 0.6 * window.innerWidth
     const height = window.innerHeight
 
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
-    const renderer = new THREE.WebGLRenderer({ alpha: true })
+    const renderer = new THREE.WebGLRenderer({
+      alpha: true,
+      antialias: true
+    })
 
     renderer.setSize(width, height)
     renderer.setClearColor(0xffffff, 0)
 
-    document.body.appendChild(renderer.domElement)
+    this.mainRef.current.appendChild(renderer.domElement)
 
     const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    const material = new THREE.MeshStandardMaterial({ color: 0x800080 })
 
     const cube = new THREE.Mesh(geometry, material)
     scene.add(cube)
+
+    const light = new THREE.DirectionalLight(0xffffff, 5.0)
+    light.position.set(10, 10, 10)
+    scene.add(light)
 
     camera.position.z = 5
 
@@ -30,9 +42,16 @@ export default class ThreeDemo extends Component {
       renderer.render(scene, camera)
     }
     animate()
+
+    // Make Responsive
+    window.addEventListener('resize', () => {
+      camera.aspect = (0.6 * window.innerWidth) / window.innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(0.6 * window.innerWidth, window.innerHeight)
+    })
   }
 
   render() {
-    return <div></div>
+    return <div ref={this.mainRef} />
   }
 }
